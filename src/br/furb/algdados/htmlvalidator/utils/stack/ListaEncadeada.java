c6a7edc046e58e0dbea3d94486723f56ae6fb39e
Lucas Samuel <lucasskluser@outlook.com>
2020-05-10 00:57:03 -0300

    chore: initial commit

---
+++ src/br/furb/algdados/htmlvalidator/utils/stack/ListaEncadeada.java
@@ -0,0 +1,94 @@
+package br.furb.algdados.htmlvalidator.utils.stack;
+
+public class ListaEncadeada<T> {
+    private NoLista<T> primeiro;
+    private int tamanho;
+
+    public void inserir(T info) {
+        NoLista<T> noLista = new NoLista<>();
+        noLista.setInfo(info);
+
+        if(primeiro != null) {
+            noLista.setProximo(primeiro);
+        }
+
+        primeiro = noLista;
+        tamanho++;
+    }
+
+    public boolean estaVazia() {
+        return primeiro == null;
+    }
+
+    public NoLista<T> buscar(T info){
+        NoLista<T> noLista = primeiro;
+
+        while (noLista != null) {
+            if (noLista.getInfo() == info) {
+                return noLista;
+            }
+
+            noLista = noLista.getProximo();
+        }
+
+        return null;
+    }
+
+    public void retirar(T info) {
+        NoLista<T> noLista = primeiro;
+        NoLista<T> noAnterior = noLista;
+
+        while (noLista != null){
+            if(noLista.getInfo() == info) {
+                break;
+            }
+
+            noAnterior = noLista;
+            noLista = noLista.getProximo();
+        }
+
+        noAnterior.setProximo(noLista.getProximo());
+        tamanho--;
+    }
+
+    public int obterComprimento() {
+        return tamanho;
+    }
+
+    public NoLista<T> obterNo(int idx) {
+        if(idx < 0 || idx > obterComprimento()){
+            throw new IndexOutOfBoundsException();
+        }
+
+        NoLista<T> noLista = primeiro;
+        int i = 0;
+
+        while(noLista != null) {
+            if (i == idx){
+                break;
+            }
+
+            noLista = noLista.getProximo();
+            i++;
+        }
+
+        return noLista;
+    }
+
+    @Override
+    public String toString() {
+        String string = "";
+        NoLista<T> noLista = primeiro;
+
+        while (noLista != null){
+            if (string.length() > 0) {
+                string += ", ";
+            }
+
+            string += noLista.getInfo();
+            noLista = noLista.getProximo();
+        }
+
+        return string;
+    }
+}
---
+++ src/br/furb/algdados/htmlvalidator/utils/stack/NoLista.java
@@ -0,0 +1,22 @@
+package br.furb.algdados.htmlvalidator.utils.stack;
+
+public class NoLista<T> {
+    private T info;
+    private NoLista<T> proximo;
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public NoLista<T> getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(NoLista<T> proximo) {
+        this.proximo = proximo;
+    }
+}
