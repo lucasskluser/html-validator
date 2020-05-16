bc6cece1d784850c4039ffc963ac789a4bb66c92
matheusboing <matheus.boing@hbsis.com.br>
2020-05-16 18:32:21 -0300

    feat: controlador e view principal

---
+++ src/br/furb/algdados/htmlvalidator/controllers/HTMLController.java
@@ -0,0 +1,76 @@
+package br.furb.algdados.htmlvalidator.controllers;
+
+import br.furb.algdados.htmlvalidator.exceptions.HTMLBadFormattingException;
+import br.furb.algdados.htmlvalidator.exceptions.NotHTMLFileException;
+import br.furb.algdados.htmlvalidator.services.HTMLReaderService;
+import br.furb.algdados.htmlvalidator.services.HTMLValidatorService;
+import br.furb.algdados.htmlvalidator.views.MainView;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+
+public class HTMLController {
+    private MainView mainView;
+    private String openedFile;
+    private HTMLValidatorService htmlValidatorService;
+    private HTMLReaderService htmlReaderService;
+
+    public HTMLController() {
+        mainView = new MainView(this);
+        htmlValidatorService = new HTMLValidatorService();
+        htmlReaderService = new HTMLReaderService();
+    }
+
+    public void view() {
+        mainView.show();
+    }
+
+    public void selectFile(String filePath) {
+        try {
+            File file = new File(filePath);
+            JFileChooser fileChooser = new JFileChooser();
+
+            if(file.isDirectory() || openedFile.equals(filePath)) {
+                fileChooser.setCurrentDirectory(file);
+                fileChooser.setFileFilter(new FileNameExtensionFilter("HTML", "html"));
+
+                int returnVal = fileChooser.showOpenDialog(mainView.getJPanel());
+
+                if(returnVal == JFileChooser.APPROVE_OPTION) {
+                    file = htmlReaderService.readFile(fileChooser.getSelectedFile().getAbsolutePath());
+                } else if (returnVal == JFileChooser.CANCEL_OPTION){
+                    return;
+                }
+            }
+
+            openedFile = file.getAbsolutePath();
+            validateFile(file);
+        } catch (NotHTMLFileException e) {
+            showError("O arquivo não está no formato HTML!");
+        } catch (FileNotFoundException e) {
+            showError("Não foi possível encontrar o arquivo!");
+        } catch (Throwable throwable) {
+            showError("Não foi possível carregar o arquivo!");
+        }
+    }
+
+    public void showInfo(String message) {
+        mainView.showInfo(message);
+    }
+
+    public void showError(String message) {
+        mainView.showError(message);
+    }
+
+    public void validateFile(File file) {
+        try {
+            HashMap<String, Integer> tagsMap = htmlValidatorService.validate(file);
+            mainView.loadResult(openedFile, tagsMap);
+        } catch (HTMLBadFormattingException e) {
+            mainView.loadResult(openedFile, e.getTagsMap(), e.getMessage());
+        }
+    }
+}
---
+++ src/br/furb/algdados/htmlvalidator/views/MainView.java
@@ -0,0 +1,158 @@
+package br.furb.algdados.htmlvalidator.views;
+
+import br.furb.algdados.htmlvalidator.controllers.HTMLController;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+import java.awt.event.ActionEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainView {
+    private JTextField textFieldFile;
+    private JButton buttonAnalyseFile;
+    private JTextArea textAreaResult;
+    private JLabel labelFile;
+    private JTable tableTagsFrequency;
+    private JScrollPane jtableScrollPane;
+    private JPanel jPanel;
+
+    private DefaultTableModel tableModel;
+    private HTMLController controller;
+
+    public MainView(HTMLController controller) {
+        createUIComponents();
+        createListeners();
+        this.controller = controller;
+    }
+
+    public JPanel getJPanel() {
+        return this.jPanel;
+    }
+
+    /**
+     * Exibe o JFrame
+     */
+    public void show() {
+        JFrame frame = createJFrame();
+        textFieldFile.setText(System.getProperty("user.dir"));
+        frame.setVisible(true);
+    }
+
+    /**
+     * Exibe uma mensagem de informação
+     * @param message Mensagem a ser exibida
+     */
+    public void showInfo(String message) {
+        JOptionPane.showMessageDialog(jPanel, message, null, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    /**
+     * Exibe uma mensagem de erro
+     * @param message Mensagem a ser exibida
+     */
+    public void showError(String message) {
+        JOptionPane.showMessageDialog(jPanel, message, "Ops!", JOptionPane.ERROR_MESSAGE);
+    }
+
+    /**
+     * Carrega o resultado da validação na view e preenche
+     * a tabela de tags com suas frequências
+     * @param absolutePath Caminho absoluto do arquivo validado
+     * @param tagsMap Mapa de tags com suas frequências
+     */
+    public void loadResult(String absolutePath, HashMap<String, Integer> tagsMap) {
+        loadResult(absolutePath, tagsMap, null);
+    }
+
+    /**
+     * Carrega o resultado da validação na view e preenche
+     * a tabela de tags com suas frequências
+     * @param absolutePath Caminho absoluto do arquivo validado
+     * @param tagsMap Mapa de tags com suas frequências
+     * @param validationError Erro da validação
+     */
+    public void loadResult(String absolutePath, HashMap<String, Integer> tagsMap, String validationError) {
+        textFieldFile.setText(absolutePath);
+
+        if (validationError != null) {
+            textAreaResult.setText(String.format("Erro de formatação: %s", validationError));
+        } else {
+            textAreaResult.setText("O arquivo está bem formatado");
+        }
+
+        loadTagsInTable(tagsMap);
+    }
+
+    /**
+     * Preenche a tabela de tags com suas frequências
+     * @param tagsMap Mapa de tags com suas frequências
+     */
+    private void loadTagsInTable(HashMap<String, Integer> tagsMap) {
+        tableModel.setRowCount(0);
+
+        for (Map.Entry<String, Integer> entry : tagsMap.entrySet()) {
+            tableModel.addRow(new Object[] { entry.getKey(), entry.getValue() });
+            System.out.println(String.format("TAG %s apareceu %d vezes", entry.getKey(), entry.getValue()));
+        }
+    }
+
+    /**
+     * Instancia um novo JFrame
+     * @return Novo JFrame
+     */
+    private JFrame createJFrame() {
+        JFrame frame = new JFrame();
+        frame.setContentPane(jPanel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setTitle("Validador de HTML - Lucas Samuel & Matheus Boing");
+        frame.setAutoRequestFocus(true);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+
+        return frame;
+    }
+
+    /**
+     * Instancia componentes customizados da view
+     */
+    private void createUIComponents() {
+        Object[] columns = { "Tag", "Frequência" };
+        tableModel = new DefaultTableModel(columns, 0);
+        tableTagsFrequency = new JTable(tableModel);
+        jtableScrollPane = new JScrollPane(tableTagsFrequency);
+    }
+
+    /**
+     * Define os listeners de ação
+     */
+    private void createListeners() {
+        buttonAnalyseFile.addActionListener(e -> buttonAnalyseFileClickListener());
+
+        textFieldFile.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    textFieldFileOnEnterPressListener();
+                } catch (Exception ex) {
+                    showError("Não foi possível processar o caminho do arquivo");
+                }
+            }
+        });
+    }
+
+    private void buttonAnalyseFileClickListener() {
+        if (textFieldFile.getText().length() > 0) {
+            controller.selectFile(textFieldFile.getText());
+        }
+    }
+
+    private void textFieldFileOnEnterPressListener() {
+        buttonAnalyseFileClickListener();
+    }
+}
