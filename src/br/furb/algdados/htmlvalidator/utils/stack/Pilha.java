e3845bdfd7af396ca1ca84989ae0c1bfd633d104
matheusboing <matheus.boing@hbsis.com.br>
2020-05-16 10:20:03 -0300

    feat: implementacao pilha lista

--- src/br/furb/algdados/htmlvalidator/utils/stack/Pilha.java
+++ src/br/furb/algdados/htmlvalidator/utils/stack/Pilha.java
@@ -1,4 +1,9 @@
 package br.furb.algdados.htmlvalidator.utils.stack;
 
-public interface Pilha {
+public interface Pilha<T> {
+    void push(T info);
+    T pop();
+    T peek();
+    boolean estaVazia();
+    void liberar();
 }
--- src/br/furb/algdados/htmlvalidator/utils/stack/PilhaLista.java
+++ src/br/furb/algdados/htmlvalidator/utils/stack/PilhaLista.java
@@ -1,4 +1,45 @@
 package br.furb.algdados.htmlvalidator.utils.stack;
 
-public class PilhaLista {
+import br.furb.algdados.htmlvalidator.utils.list.ListaEncadeada;
+
+public class PilhaLista<T> implements Pilha<T> {
+    private ListaEncadeada<T> lista;
+
+    public PilhaLista() {
+        lista = new ListaEncadeada();
+    }
+
+    @Override
+    public void push(T info) {
+        lista.inserir(info);
+    }
+
+    @Override
+    public T pop() {
+        if(estaVazia()) {
+            throw new PilhaVaziaException();
+        }
+
+        T info = peek();
+        lista.retirar(info);
+        return info;
+    }
+
+    @Override
+    public T peek() {
+        T info = lista.obterNo(0).getInfo();
+        return info;
+    }
+
+    @Override
+    public boolean estaVazia() {
+        return lista.estaVazia();
+    }
+
+    @Override
+    public void liberar() {
+        while (estaVazia() == false){
+            pop();
+        }
+    }
 }
